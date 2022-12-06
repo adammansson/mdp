c86501c22331506bbe39b3497b8f91693c894f4e
Adam Månsson <adam.s.mansson@gmail.com>
2022-12-06 19:44:52 +0100

    Clean up temp files

--- main.go
+++ main.go
@@ -4,13 +4,12 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
-
 	"io"
 	"io/ioutil"
 	"os"
-
 	"os/exec"
 	"runtime"
+	"time"
 
 	"github.com/microcosm-cc/bluemonday"
 	"github.com/russross/blackfriday/v2"
@@ -72,6 +71,9 @@ func run(filename string, out io.Writer, skipPreview bool) error {
 	if skipPreview {
 		return nil
 	}
+
+	defer os.Remove(outName)
+
 	return preview(outName)
 }
 
@@ -113,5 +115,8 @@ func preview(fname string) error {
 		return err
 	}
 
-	return exec.Command(cPath, cParams...).Run()
+	err = exec.Command(cPath, cParams...).Run()
+
+	time.Sleep(2 * time.Second)
+	return err
 }
